676fdf86022fb89003bec75aca36a5fdb1591cf5
SmitModi-7 <smitmodi.ict18@gmail.com>
2024-11-03 21:14:48 +0530

    Deploying application through vercel

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 
 application = get_wsgi_application()
+
+# To connect our app to vercel app
+app = application
